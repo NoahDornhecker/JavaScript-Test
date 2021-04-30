faeed7941fc62a31b314a26e5db18bacd4169194
NoahDornhecker <66961367+NoahDornhecker@users.noreply.github.com>
2021-04-30 14:48:32 +0200

    Delete test3.js

--- test3.js
+++
@@ -1,5 +0,0 @@
-var r = document.getElementById('ext-gen101').contentWindow.document.getElementById('ext-gen39')
-r.addEventListener("click", function() {
-	console.log("Online Click");
-	chrome.runtime.sendMessage({message: "R"});
-});
